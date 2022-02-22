5d52ee33a636219e796ffb69c515e2c5de076d9f
SongLiKod <1321089229@qq.com>
2022-02-22 15:24:47 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mkdir ~/.screen && chmod 777 ~/.screen
+mkdir ~/.screen && chmod 700 ~/.screen
 export SCREENDIR=$HOME/.screen
-chmod u+x /usr/bin/ttyd
 /usr/bin/ttyd -p $PORT -c admin:admin8888 bash
